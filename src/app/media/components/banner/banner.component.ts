11920bb6605c6d69383e144e70bb3bbcad59dfec
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2021-09-29 18:52:16 +0100

    fix(*): make sure assets have correct url for staging

--- src/app/media/components/banner/banner.component.ts
+++ src/app/media/components/banner/banner.component.ts
@@ -1,11 +1,11 @@
-import { Component, ElementRef, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'aa-banner',
     styleUrls: ['./banner.component.scss'],
     templateUrl: './banner.component.html'
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit {
     @Input()
     public set image(image: string) {
         (this._element.nativeElement as HTMLElement).style.setProperty('--aaBanner-image', `url("${image}")`);
@@ -18,5 +18,9 @@ export class BannerComponent {
 
     constructor(element: ElementRef) {
         this._element = element;
+    }
+
+    public ngOnInit(): void {
+        (this._element.nativeElement as HTMLElement).style.setProperty('--aaBanner-overlayImage', 'url(assets/bg.jpg)');
     }
 }
